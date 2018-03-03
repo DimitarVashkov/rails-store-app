b0bf57bfc8fdde305dedfe94c1c28cecd273a968
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-03 14:25:43 +0000

    Basic cart creation

---
+++ app/models/line_item.rb
@@ -0,0 +1,9 @@
+class LineItem < ApplicationRecord
+  # product:references and cart:belongs_to
+  # are the same at model level
+  #
+  # If a table has any columns whose values consist of ID values
+  # for another table, the model should have a belongs_to for each
+  belongs_to :product
+  belongs_to :cart
+end
